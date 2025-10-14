d75f1bd0e2697f7be10a151f50cd0148f6723566
Happy-Duck <66819856+Happy-Duck@users.noreply.github.com>
2025-10-14 01:42:43 -0500

    Base Structure
    Initialized the Docker stuff and a basic functioning discord bot

---
+++ src/bot.py
@@ -0,0 +1,32 @@
+#The main bot script
+
+# We definitely need these at the very least
+import discord
+from discord import app_commands
+
+TOKEN = 'MTQyNzUyNzc1MzE1NDE3MTAyMA.GOrQcs.JF_0mon0ggzsq9tb30C5iDmfM4bJLIgAP-RDiU'
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+# This is temporary (borrowed) for testing if im doing any of this right
+@client.event
+async def on_ready():
+    print('We have successfully loggged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == 'hello':
+        await message.channel.send(f'Hello, {message.author.display_name}!')
+        return
+
+    if message.content.lower() == 'bye':
+        await message.channel.send(f'See you later, {message.author.display_name}!')
+        return
+
+client.run(TOKEN)
